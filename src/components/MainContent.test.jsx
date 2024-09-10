3213ab4d337d467c1bf123abe501eb14eb6cc454
Adam <Adamtaylor1848@gmail.com>
2024-09-10 19:59:20 +0100

    just for giggles note

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -1,7 +1,7 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-
+//just for giggles
 import MainContent from './MainContent';
 
 describe('MainContent', () => {
